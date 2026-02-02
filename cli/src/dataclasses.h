d13939ad28a78e5b0864ff91b3aa0ea7266bbbf6
Pobarueva Sofiya <s_ext.spobarueva@m1-rnd-graphar-dev.m1.rnd-processing.local>
2026-02-02 17:05:47 +0300

    refactor: moved dataclasses to dataclasses.h, additional functions with data and convertation moved to tools.h (.cpp later)

---
+++ cli/src/dataclasses.h
@@ -0,0 +1,93 @@
+/*
+* This file contains data structures used to parse configs 
+* and functions that do parsing.
+*/
+
+#pragma once
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+struct GraphArConfig {
+  std::string path;
+  std::string name;
+  std::string version;
+};
+
+struct Property {
+  std::string name;
+  std::string data_type;
+  bool is_primary;
+  bool nullable;
+};
+
+struct PropertyGroup {
+  std::string file_type;
+  std::vector<Property> properties;
+};
+
+struct Source {
+  std::string file_type;
+  std::vector<std::string> path;
+  char delimiter;
+  std::unordered_map<std::string, std::string> columns;
+};
+
+struct Vertex {
+  std::string type;
+  std::vector<std::string> labels;
+  int chunk_size;
+  std::string validate_level;
+  std::string prefix;
+  std::vector<PropertyGroup> property_groups;
+  std::vector<Source> sources;
+};
+
+struct MergeVertex: public Vertex {
+    std::string join_on;
+};
+
+struct AdjList {
+  bool ordered;
+  std::string aligned_by;
+  std::string file_type;
+};
+
+struct Edge {
+  std::string edge_type;
+  std::string src_type;
+  std::string src_prop;
+  std::string src_edge_prop;
+  std::string dst_type;
+  std::string dst_prop;
+  std::string dst_edge_prop;
+  int chunk_size;
+  std::string validate_level;
+  std::string prefix;
+  std::vector<AdjList> adj_lists;
+  std::vector<PropertyGroup> property_groups;
+  std::vector<Source> sources;
+};
+
+struct ImportSchema {
+  std::vector<Vertex> vertices;
+  std::vector<Edge> edges;
+};
+
+struct MergeSchema {
+    std::vector<MergeVertex> vertices;
+    std::vector<Edge> edges;
+};
+
+struct MergeConfig {
+  GraphArConfig graphar_config;
+  MergeSchema merge_schema;
+  bool debug_mode;
+};
+
+struct ImportConfig {
+  GraphArConfig graphar_config;
+  ImportSchema import_schema;
+  bool debug_mode;
+};
